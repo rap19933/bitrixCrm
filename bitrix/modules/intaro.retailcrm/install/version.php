2af2d1b6643603d67f33582e41b96856f5d38342
matyashov <matyashov@intaro.email>
2018-02-16 11:00:34 +0300

    start

---
+++ bitrix/modules/intaro.retailcrm/install/version.php
@@ -0,0 +1,7 @@
+
+<?
+$arModuleVersion = array(
+    "VERSION" => "2.3.3",
+    "VERSION_DATE" => "2018-01-24 09:26:21"
+);
+
---
+++ bitrix/php_interface/init.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ser
+ * Date: 16.02.18
+ * Time: 10:30
+ */
